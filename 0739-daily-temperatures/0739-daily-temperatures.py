f9c3f57893503dae8e5904b8821227c6ab943b54
Dabby <99619796+DabbyEmezue@users.noreply.github.com>
2023-10-05 12:27:45 +1100

    Time: 1032 ms (88.81%), Space: 30.4 MB (85.66%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,7 +1,7 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         Stack=[]
-        ans = [0 for _ in range(len(temperatures))]
+        ans = [0] *len(temperatures)
         for i in range(len(temperatures)):
             if len(Stack)==0:
                 Stack.append(i)
