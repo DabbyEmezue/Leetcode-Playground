4e734aeedcbcae4521107f46f7f963bcc709a362
Dabby <99619796+DabbyEmezue@users.noreply.github.com>
2023-10-23 23:19:45 +1100

    Time: 134 ms (31.48%), Space: 16.2 MB (97.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,15 +3,17 @@ class Solution:
         count ={}
         for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
             count[letter]=0
-        result = 0
+            result = 0
         L = 0
-        for R in range(len(s)):
+        R = 0
+        while R < len(s):
             count[s[R]]+=1
             maxf=max(count.values())
             
             if R-L+1 - maxf > k:
                 count[s[L]]-=1
                 L+=1
-            result = max(result,R-L+1)
-           
+                
+            result = max(result, R - L + 1)
+            R+=1
         return result
